41d26938b881a23976070a53ba08bf6ef0883858
Iryna Ivashchenko <irinasmile@ukr.net>
2021-07-03 12:29:49 +0300

    1

--- src/js/task-01.js
+++ src/js/task-01.js
@@ -1,7 +1,7 @@
-const allCategoriesEl = document.querySelectorAll(".item");
+const allCategories = document.querySelectorAll(".item");
 console.log("В списке", allCategoriesEl.length, "категорий"); // В списке 3 категорий
 
-allCategoriesEl.forEach((num) => {
+allCategories.forEach((num) => {
   console.log("Категория:", num.firstElementChild.textContent);
   console.log("Количество элементов:", num.lastElementChild.children.length);
 });
