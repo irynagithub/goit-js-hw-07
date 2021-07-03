0eb282c920c3499aaa57849fabd27a7721301635
Iryna Ivashchenko <irinasmile@ukr.net>
2021-07-03 12:08:40 +0300

    2

--- src/js/task-02.js
+++ src/js/task-02.js
@@ -1,8 +1,47 @@
 const ingredients = [
-  'Картошка',
-  'Грибы',
-  'Чеснок',
-  'Помидоры',
-  'Зелень',
-  'Приправы',
+  "Картошка",
+  "Грибы",
+  "Чеснок",
+  "Помидоры",
+  "Зелень",
+  "Приправы",
 ];
+const addIngredients = document.querySelector("#ingredients");
+
+const newItem = (elem) => {
+  const itemEl = document.createElement("li");
+  itemEl.textContent = elem;
+  return itemEl;
+};
+const ingredientsArray = ingredients.map((ingredient) => newItem(ingredient));
+addIngredients.append(...ingredientsArray);
+
+// for (let elem of ingredients) {
+//   const itemEl = document.createElement('li');
+//   itemEl.textContent = elem;
+//   addIngredients.append(itemEl);
+// }
+
+//===============================================================
+
+// const elements = ingredients.map(option => {
+//   const itemEl = document.createElement('li');
+//   itemEl.textContent = option;
+//   const listEl = document.querySelector('#ingredients');
+//   listEl.appendChild(itemEl);
+//   return itemEl;
+// });
+// console.log(elements);
+
+// ============= old school ==================
+
+// const elements = [];
+// for (let i = 0; i < ingredients.length; i += 1) {
+//   const option = ingredients[i];
+//   const itemEl = document.createElement('li');
+//   itemEl.textContent = option;
+//   const listEl = document.querySelector('#ingredients');
+//   listEl.appendChild(itemEl);
+//   elements.push(itemEl);
+// }
+// console.log(elements);
